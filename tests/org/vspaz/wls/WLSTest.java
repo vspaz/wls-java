ed767c7f295b635f04a772cef3983379f1f0c783
vspaz <vladimir.edelman@gmail.com>
2023-01-25 01:43:45 +0100

    minor test fixes

--- tests/org/vspaz/wls/WLSTest.java
+++ tests/org/vspaz/wls/WLSTest.java
@@ -6,34 +6,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class WLSTest {
 
+  final double delta = 1.0e-6;
+
   @Test
   void testWLSModelStableWeight() {
-    double[] x = {1, 2, 3, 4, 5, 6, 7};
-    double[] y = {1, 3, 4, 5, 2, 3, 4};
+    final double[] x = {1, 2, 3, 4, 5, 6, 7};
+    final double[] y = {1, 3, 4, 5, 2, 3, 4};
 
     WLS wls = new WLS(x, y);
     Point point = wls.fitLinearRegression();
 
-    assertEquals(2.14285714, point.getIntercept(), 6);
-    assertEquals(0.25, point.getSlope(), 6);
+    assertEquals(2.14285714, point.getIntercept(), delta);
+    assertEquals(0.25, point.getSlope(), delta);
   }
 
   @Test
   void testWLSModelWeight() {
-    double[] x = {1, 2, 3, 4, 5, 6, 7};
-    double[] y = {1, 3, 4, 5, 2, 3, 4};
+    final double[] x = {1, 2, 3, 4, 5, 6, 7};
+    final double[] y = {1, 3, 4, 5, 2, 3, 4};
 
     WLS wls = new WLS(x, y, 0.9);
     Point point = wls.fitLinearRegression();
 
-    assertEquals(2.14285714, point.getIntercept(), 6);
-    assertEquals(0.25, point.getSlope(), 6);
+    assertEquals(2.14285714, point.getIntercept(), delta);
+    assertEquals(0.25, point.getSlope(), delta);
   }
 
   @Test()
   void testSinglePointDisallowed() {
-    double[] x = {10};
-    double[] y = {1};
+    final double[] x = {10};
+    final double[] y = {1};
     assertThrows(
         AssertionError.class,
         () -> new WLS(x, y));
